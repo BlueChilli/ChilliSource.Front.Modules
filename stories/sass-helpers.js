4abf6c4872f10c92b6851e9582ab738a62d78484
Parth Mehta <tech@iamdifferent.io>
2018-07-10 08:13:46 +1000

    added TextStyles

--- stories/sass-helpers.js
+++ stories/sass-helpers.js
@@ -14,8 +14,36 @@ import "../sass-helpers/index.css";
 
 /** Rendering Components */
 storiesOf("Style Helpers", module)
+  .add("Text", () => {
+    return (
+      <React.Fragment>
+        <h1>Text Styles</h1>
+
+        <br />
+        <h1>This is an h1 heading</h1>
+
+        <br />
+        <h2>This is an h2 heading</h2>
+
+        <br />
+        <h3>This is an h3 heading</h3>
+
+        <br />
+        <h4>This is an h4 heading</h4>
+
+        <br />
+        <h5>This is an h5 heading</h5>
+
+        <br />
+        <h6>This is an h6 heading</h6>
+
+        <br />
+        <p>This is a paragraph text</p>
+      </React.Fragment>
+    );
+  })
   .addDecorator(getStory => <DemoFormDecorator story={getStory()} />)
-  .add("Forms", () => {
+  .add("Forms Components", () => {
     return (
       <React.Fragment>
         <h1 className="margin-bottom-2">Form Components</h1>
