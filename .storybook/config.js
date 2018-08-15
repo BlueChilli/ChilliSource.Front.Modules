c68bf68be0692ab4e4fb0ea4cc4f07041abe5c7f
Parth Mehta <tech@iamdifferent.io>
2018-08-15 12:26:25 +1000

    :wrench: updated folder names

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,10 @@
-import {configure} from '@storybook/react';
+import { configure } from '@storybook/react';
 
 function loadStories() {
-  require('../stories/form-vertical-fields.js');
-  require('../stories/sass-helpers.js');
-  require('../stories/components.js');
-  require('../stories/chillsource-app.js');
-
-
-
+	require('../stories/form-vertical-fields.js');
+	require('../stories/style-helpers.js');
+	require('../stories/components.js');
+	require('../stories/chillsource-app.js');
 }
 
 configure(loadStories, module);
