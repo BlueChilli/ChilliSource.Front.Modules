9a5be2dc2991d758f2ab21bd1f17ac937f1f1aba
Parth Mehta <tech@iamdifferent.io>
2018-08-22 14:05:16 +1000

    :bug: more minor fixes

--- stories/chillsource-app.js
+++ stories/chillsource-app.js
@@ -1,47 +1,54 @@
-import React, {Fragment} from 'react';
-import {storiesOf} from '@storybook/react';
-import "./helpers/storybook.css";
-import chillifront from "chillifront";
-import configureStore from "../chillisauce-app/redux/configureStore";
-import NotificationTest from "../chillisauce-app/App/NotificationTest";
-import NotFoundPage from "../modules/404/index";
-import ReduxThunk from "../modules/ReduxThunk/index";
-import Notification from "../modules/Notification/index";
-import GoogleAnalytics from "../modules/GoogleAnalytics/index";
-import Ga from "../chillisauce-app/App/Ga";
-import ReduxPromiseMiddleware from "../modules/ReduxPromiseMiddleware/index";
-import SwaggerData from "../modules/ReduxSwagger/index";
-import SwaggerDataTest from "../chillisauce-app/App/SwaggerDataTest";
+import React, { Fragment } from 'react';
+import { storiesOf } from '@storybook/react';
+import './helpers/storybook.css';
+import chillifront from 'chillifront';
+import configureStore from '../chillisauce-app/redux/configureStore';
+import NotificationTest from '../chillisauce-app/App/NotificationTest';
+import NotFoundPage from '../modules/404/index';
+import ReduxThunk from '../modules/ReduxThunk/index';
+import Notification from '../modules/Notification/index';
+import GoogleAnalytics from '../modules/GoogleAnalytics/index';
+import Ga from '../chillisauce-app/App/Ga';
+import ReduxPromiseMiddleware from '../modules/ReduxPromiseMiddleware/index';
+import SwaggerData from '../modules/ReduxSwagger/index';
+import SwaggerDataTest from '../chillisauce-app/App/SwaggerDataTest';
 
-const CF = ({children}) => {
-  const C = children;
-  return <C/>;
+const CF = ({ children }) => {
+	const C = children;
+	return <C />;
 };
 
 storiesOf('ChillFront', module)
-  .add('Notifications', () => <CF>{chillifront(
-    [
-      new Notification(),
-      new ReduxThunk(),
-      new NotFoundPage()
-    ],
-    configureStore
-  )(NotificationTest)}</CF>)
-  .add('Google Analytics', () => <CF>{chillifront(
-    [
-      new GoogleAnalytics({
-        debug: true,
-        trackingId: "UA-103046519-2",
-        require: ['eventTracker', 'outboundLinkTracker', 'urlChangeTracker']
-      })
-    ],
-    configureStore
-  )(Ga)}</CF>)
-  .add('Swagger Data', () => <CF>{chillifront(
-    [
-      new ReduxPromiseMiddleware(),
-      new ReduxThunk(),
-      new SwaggerData()
-    ],
-    configureStore
-  )(SwaggerDataTest)}</CF>);
+	.add('Notifications', () => (
+		<CF>
+			{chillifront([new Notification(), new ReduxThunk(), new NotFoundPage()], configureStore)(
+				NotificationTest
+			)}
+		</CF>
+	))
+	.add('Google Analytics', () => (
+		<CF>
+			{chillifront(
+				[
+					new GoogleAnalytics({
+						debug: true,
+						trackingId: 'UA-103046519-2',
+						require: ['eventTracker', 'outboundLinkTracker', 'urlChangeTracker'],
+					}),
+				],
+				configureStore
+			)(Ga)}
+		</CF>
+	))
+	.add('Swagger Data', () => (
+		<CF>
+			{chillifront(
+				[
+					new ReduxPromiseMiddleware(),
+					new ReduxThunk(),
+					// new SwaggerData()
+				],
+				configureStore
+			)(SwaggerDataTest)}
+		</CF>
+	));
